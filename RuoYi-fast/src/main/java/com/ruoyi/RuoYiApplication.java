1f23316aee8ecfb23bd8358bda10b27cc0766d6e
wangbowei <17607147805@163.com>
2018-11-08 14:46:37 +0800

    wangbowei 第二次分支测试

--- RuoYi-fast/src/main/java/com/ruoyi/RuoYiApplication.java
+++ RuoYi-fast/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -14,10 +14,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @MapperScan("com.ruoyi.project.*.*.mapper")
 public class RuoYiApplication
 {
+
+
     public static void main(String[] args)
     {
-
-
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
